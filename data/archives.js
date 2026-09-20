8e1328f062bf5d767accacb38371569fc201961b
lltaniyall <lltaniyall.email@gmail.com>
2026-09-20 17:21:44 +0900

    Add files via upload

--- data/archives.js
+++ data/archives.js
@@ -1,5 +1,75 @@
 const ARCHIVES = [
-
+  {
+    "date": "2026-12-21",
+    "title": "冬の夜歌 / DEMO",
+    "videoId": "M7lc1UVf-VE",
+    "songs": [
+      {
+        "title": "オープニング",
+        "time": 0,
+        "type": "talk"
+      },
+      {
+        "title": "KING",
+        "artist": "Kanaria",
+        "time": 321,
+        "type": "song"
+      },
+      {
+        "title": "ロウワー",
+        "artist": "ぬゆり",
+        "time": 702,
+        "type": "song"
+      },
+      {
+        "title": "MC",
+        "time": 980,
+        "type": "talk"
+      },
+      {
+        "title": "ヴィラン",
+        "artist": "てにをは",
+        "time": 1111,
+        "type": "song"
+      }
+    ]
+  },
+  {
+    "date": "2026-12-23",
+    "title": "深夜にゆったり歌う枠 / DEMO",
+    "videoId": "M7lc1UVf-VE",
+    "songs": [
+      {
+        "title": "オープニング",
+        "time": 0,
+        "type": "talk"
+      },
+      {
+        "title": "怪獣の花唄",
+        "artist": "Vaundy",
+        "time": 245,
+        "type": "song"
+      },
+      {
+        "title": "酔いどれ知らず",
+        "artist": "Kanaria",
+        "time": 622,
+        "type": "song"
+      },
+      {
+        "title": "MC",
+        
+        "time": 955,
+        "type": "talk"
+      },
+      {
+        "title": "KING",
+        "artist": "Kanaria",
+        "time": 1284,
+        "type": "song"
+      }
+    ]
+  },
 {
 "date": "2026-07-26",
 "title": "【 歌枠 】好きな曲歌つたりのんびり雑談したりする日曜日♡【 甘萬つもり｜#個人Vtuber 】",
