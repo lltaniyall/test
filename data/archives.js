09de334efea29454594f32cf83b2722ab1230b9d
lltaniyall <lltaniyall.email@gmail.com>
2026-09-21 04:52:21 +0900

    Add files via upload

--- data/archives.js
+++ data/archives.js
@@ -1,4 +1,241 @@
 const ARCHIVES = [
+
+{
+"date": "2026-07-03",
+"membersOnly": false,
+"title": "【 歌枠 】久しぶりの歌枠♡ジヤンルごちや混ぜのリハビリお歌！【 甘萬つもり｜#個人Vtuber 】",
+"videoId": "A5Mu8Kza-e4",
+"songs": [
+{
+"title": "開始",
+"time": 75,
+"type": "talk"
+},
+{
+"title": "ワインレッドの心",
+"artist": "安全地帯",
+"time": 672,
+"type": "song"
+},
+{
+"title": "嘘",
+"artist": "シド",
+"time": 1105,
+"type": "song"
+},
+{
+"title": "メリッサ",
+"artist": "ポルノグラフィティ",
+"time": 1574,
+"type": "song"
+},
+{
+"title": "READY STEADY GO",
+"artist": "L'Arc～en～Ciel",
+"time": 2044,
+"type": "song"
+},
+{
+"title": "より酔い",
+"artist": "和ぬか",
+"time": 2921,
+"type": "song"
+},
+{
+"title": "怪獣の花唄",
+"artist": "Vaundy",
+"time": 3266,
+"type": "song"
+},
+{
+"title": "CHE.R.RY",
+"artist": "Yui",
+"time": 3608,
+"type": "song"
+},
+{
+"title": "撲殺天使ドクロちゃん",
+"artist": "ドクロちゃん(千葉紗子) ",
+"time": 3955,
+"type": "song"
+},
+{
+"title": "最強○×計画",
+"artist": "MOSAIC.WAV",
+"time": 4677,
+"type": "song"
+},
+{
+"title": "お呪い",
+"artist": "なきそ",
+"time": 5189,
+"type": "song"
+},
+{
+"title": "帰り道",
+"artist": "八九寺真宵(加藤英美里)",
+"time": 5373,
+"type": "song"
+},
+{
+"title": "もってけ!セーラーふく",
+"artist": "泉こなた(平野綾),柊かがみ(加藤英美里),柊つかさ(福原香織),高良みゆき(遠藤綾)",
+"time": 5761,
+"type": "song"
+},
+{
+"title": "モニタリング",
+"artist": "DECO*27",
+"time": 6430,
+"type": "song"
+},
+{
+"title": "あゝ無情",
+"artist": "アン・ルイス",
+"time": 7188,
+"type": "song"
+},
+{
+"title": "よくばり",
+"artist": "Ayase",
+"time": 7593,
+"type": "song"
+},
+{
+"title": "天体観測",
+"artist": "BUMP OF CHICKEN",
+"time": 7829,
+"type": "song"
+}
+]
+},
+{
+"date": "2026-07-10",
+"membersOnly": false,
+"title": "【 歌枠 】ボカロ縛りでインタアネツト老人会【 甘萬つもり｜#個人Vtuber 】",
+"videoId": "9PftB9uQQqk",
+"songs": [
+{
+"title": "開始",
+"time": 188,
+"type": "talk"
+},
+{
+"title": "吉原ラメント",
+"artist": "亜沙 feat.重音テト",
+"time": 772,
+"type": "song"
+},
+{
+"title": "Fire◎Flower",
+"artist": "halyosy",
+"time": 1209,
+"type": "song"
+},
+{
+"title": "夕立のりぼん",
+"artist": "みきとP",
+"time": 1667,
+"type": "song"
+},
+{
+"title": "酔いどれ知らず",
+"artist": "Kanaria",
+"time": 2405,
+"type": "song"
+},
+{
+"title": "スイートマジック",
+"artist": "Junky",
+"time": 2561,
+"type": "song"
+},
+{
+"title": "二息歩行",
+"artist": "DECO*27",
+"time": 3051,
+"type": "song"
+},
+{
+"title": "おじゃま虫",
+"artist": "DECO*27",
+"time": 3357,
+"type": "song"
+},
+{
+"title": "乙女解剖",
+"artist": "DECO*27",
+"time": 4152,
+"type": "song"
+},
+{
+"title": "ダブルラリアット",
+"artist": "アゴアニキ",
+"time": 4457,
+"type": "song"
+},
+{
+"title": "いろは唄",
+"artist": "銀サク",
+"time": 4792,
+"type": "song"
+},
+{
+"title": "結ンデ開イテ羅刹ト骸",
+"artist": "ハチ",
+"time": 5047,
+"type": "song"
+},
+{
+"title": "ペテン師が笑う頃に",
+"artist": "梨本うい",
+"time": 6055,
+"type": "song"
+},
+{
+"title": "曖昧エレジー",
+"artist": "DECO*27",
+"time": 6308,
+"type": "song"
+},
+{
+"title": "深海少女",
+"artist": "ゆうゆ",
+"time": 6632,
+"type": "song"
+},
+{
+"title": "ロミオとシンデレラ",
+"artist": "doriko",
+"time": 6753,
+"type": "song"
+},
+{
+"title": "プロポーズ",
+"artist": "内緒のピアス",
+"time": 7522,
+"type": "song"
+},
+{
+"title": "ラブカ？",
+"artist": "柊キライ",
+"time": 8014,
+"type": "song"
+},
+{
+"title": "絶え間なく藍色",
+"artist": "獅子志司",
+"time": 8284,
+"type": "song"
+},
+{
+"title": "からくりピエロ",
+"artist": "40mP",
+"time": 8553,
+"type": "song"
+}
+]
+},
    {
     "date": "2026-07-26",
     "membersOnly": false,
