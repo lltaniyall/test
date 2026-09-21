bef18f123a65ce44b55c9655ce016abd96ace889
lltaniyall <lltaniyall.email@gmail.com>
2026-09-21 09:58:16 +0900

    Add files via upload

---
+++ site-config.js
@@ -0,0 +1,14 @@
+const SITE_CONFIG = {
+  creator: {
+    name: "活動名",
+    catchcopy: "キャッチコピー",
+    profile: "プロフィール文"
+  },
+  socials: [
+    { label: "YouTube", sub: "VIDEOS / STREAMS", url: "https://www.youtube.com/@amn_tsumori?sub_confirmation=1" },
+    { label: "TWITTER（現X）", sub: "TWITTER", url: "https://x.com/amn_tsumori" },
+    { label: "BOOTH", sub: "SHOP", url: "https://amamam0808.booth.pm/" },
+    { label: "Bluesky", sub: "SOCIAL", url: "https://bsky.app/profile/amntmr.bsky.social" },
+    { label: "ファンボックス", sub: "FANBOX", url: "https://amn-tsumori.fanbox.cc/" }
+  ]
+};
